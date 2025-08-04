787491f7ce860fe4f2744bd03dab60d74f877c2a
Franklin Lawson <f.lawson1991@gmail.com>
2025-08-04 15:23:22 -0600

    Make <CR> autocomplete select over <C-y>

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -8,4 +8,12 @@ return {
             nerd_font_variant = 'normal',
         },
     },
+    config = function()
+        require('blink.cmp').setup({
+            keymap = {
+                ['<C-y>'] = {},
+                ['<CR>'] = {'select_and_accept'},
+            },
+       })
+    end,
 }
