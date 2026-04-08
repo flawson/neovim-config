8099c4d4952e43a9ed9e031ba0eef1ad0754d9da
Franklin Lawson <f.lawson1991@gmail.com>
2026-04-08 15:21:45 -0600

    add fzf for registers

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -187,11 +187,12 @@ require("lazy").setup({
 			vim.keymap.set({ "n", "v" }, "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
 			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[fr] Resume current search" })
 			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "[F]ind [C]ommands" })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 			vim.keymap.set("n", "<leader>fo", builtin.vim_options, { desc = "[F]ind [O]ptions" })
+			vim.keymap.set("n", "<leader>fy", builtin.registers, { desc = "[fy] Find registers" })
 
 			-- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
 			-- it is better explained there). This allows easily switching between pickers if you prefer using something else!
